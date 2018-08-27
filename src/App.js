6a0c3195f63ac9d276894244e51f3a7cb2fc9ece
Spazzlefrazzle <juuncaerlum@gmail.com>
2018-08-27 15:03:01 -0400

    Added rudimentary sidebar.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import TabletopToolsSidebar from './Sidebar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <TabletopToolsSidebar/>
         <p className="App-intro">
           Hello, world.
         </p>
+
       </div>
     );
   }
---
+++ src/Sidebar.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const routes = [
+  {
+    path: "/",
+    exact: true,
+    sidebar: () => <div>home!</div>,
+    main: () => <h2>Home</h2>
+  },
+  {
+    path: "/hp-tracker",
+    sidebar: () => <div>hp-tracker</div>,
+    main: () => <h2>hp-tracker</h2>
+  }
+];
+
+const TabletopToolsSidebar = () => (
+  <Router>
+    <div style={{ display: "flex" }}>
+      <div
+        style={{
+          padding: "10px",
+          width: "40%",
+          background: "#f0f0f0"
+        }}
+      >
+        <ul style={{ listStyleType: "none", padding: 0}}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/hp-tracker">HP Tracker</Link>
+          </li>
+        </ul>
+
+        {routes.map((route, index) => (
+          <Route
+            key={index}
+            path={route.path}
+            exact={route.exact}
+            component={route.siebar}
+          />
+        ))}
+      </div>
+    </div>
+  </Router>
+);
+
+export default TabletopToolsSidebar;
