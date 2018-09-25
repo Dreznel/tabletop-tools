9444ef72ebea4390c2a009a8357141a77d348b2e
Spazzlefrazzle <juuncaerlum@gmail.com>
2018-09-25 13:58:58 -0400

    Creating monster list.
    Application now populates a list of monsters. Selection still isn't implemented, and I'm a bit stuck on how to reduce the height of the monster list. It's a start, though!

--- src/hitPointTracker/MonsterSelector.js
+++ src/hitPointTracker/MonsterSelector.js
@@ -7,7 +7,31 @@ import InputLabel from '@material-ui/core/InputLabel'
 import InputAdornment from '@material-ui/core/InputAdornment'
 import TextField from '@material-ui/core/TextField'
 
+import Table from '@material-ui/core/Table'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow'
+import Paper from '@material-ui/core/Paper'
+import {withStyles} from '@material-ui/core/styles'
+
 import { connect } from 'react-redux'
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    /*
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+    float: 'left'
+    */
+  },
+  table: {
+    minWidth: 300,
+    maxHeight: 10,
+    overflowY: 'auto'
+  }
+});
 
 const mapDispatchToProps = (dispatch, props) => {
   return(
@@ -30,7 +54,8 @@ class MonsterSelector extends Component {
       monsterId : 1,
       monsterName : 'DEFAULT_NAME',
       monsterMaxHp : 100,
-      monsterCurrentHp : 100
+      monsterCurrentHp : 100,
+      monsterNamesObject: []
     };
 
     //Are these lines necessary?
@@ -62,7 +87,21 @@ class MonsterSelector extends Component {
      })
    }
 
+   fetchMonsterNames() {
+     fetch('http://www.dnd5eapi.co/api/monsters/')             //Call the endpoint, get the HTTP response
+     .then(response => response.json())                        //From the HTTP response, pull out the JSON
+     .then(responseData => {                                   //From the JSON, pull out the stuff we need.
+       this.setState(
+         {
+           monsterNamesObject: responseData.results
+         }
+       )
+     })
+   }
+
   render() {
+    const { classes } = this.props;
+    this.fetchMonsterNames();
     return (
       <div className = 'monster-selector'>
         <TextField
@@ -83,9 +122,38 @@ class MonsterSelector extends Component {
           onChange = { this.handleSelectionChange('monsterId') }
         />
 
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>Monster Name</TableCell>
+                <TableCell>Import</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.state.monsterNamesObject.map(monster => {
+                return(
+                  <TableRow key={monster.name}>
+                    <TableCell>{monster.name}</TableCell>
+                    <TableCell>
+                      <Button
+                        variant="fab"
+                        color = "secondary"
+                        aria-label="Import"
+                        className={ this.props.button }
+                        onClick={ this.importMonster }
+                        mini
+                      />
+                    </TableCell>
+                  </TableRow>
+                )
+              })}
+            </TableBody>
+          </Table>
+        </Paper>
       </div>
     )
   }
 }
 
-export default connect(null, mapDispatchToProps)(MonsterSelector);
+export default connect(null, mapDispatchToProps)(withStyles(styles)(MonsterSelector));
